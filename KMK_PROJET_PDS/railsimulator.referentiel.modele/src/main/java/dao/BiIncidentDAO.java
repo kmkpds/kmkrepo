3410292039bb683483f2b4b367adcfdb43b2b504
Alex <Alex@Alex-PC>
2013-05-15 19:59:49 +0200

    BIdao

--- KMK_PROJET_PDS/railsimulator.referentiel.modele/src/main/java/dao/BiIncidentDAO.java
+++ KMK_PROJET_PDS/railsimulator.referentiel.modele/src/main/java/dao/BiIncidentDAO.java
@@ -27,7 +27,7 @@ public class BiIncidentDAO {
 
 	public int getNbIncidentParLigne(int idtype,int annee,int idligne, String affichage) {
 		int result= 0;
-		
+
 		if(affichage.equals("annuelle")){
 			se = HibernateUtils.getSession();
 			Query query = se.createSQLQuery(
@@ -35,7 +35,7 @@ public class BiIncidentDAO {
 					.setParameter(0, idtype)
 					.setParameter(1, annee)
 					.setParameter(2, idligne);	        
-			 result =   ((BigInteger) query.uniqueResult()).intValue();		
+			result =   ((BigInteger) query.uniqueResult()).intValue();		
 		}
 		else if(affichage.equals("trimestre 1")){
 			se = HibernateUtils.getSession();
@@ -44,7 +44,7 @@ public class BiIncidentDAO {
 					.setParameter(0, idtype)
 					.setParameter(1, annee)
 					.setParameter(2, idligne);	        
-			 result =   ((BigInteger) query.uniqueResult()).intValue();	
+			result =   ((BigInteger) query.uniqueResult()).intValue();	
 		}
 		else if(affichage.equals("trimestre 2")){
 			se = HibernateUtils.getSession();
@@ -53,7 +53,7 @@ public class BiIncidentDAO {
 					.setParameter(0, idtype)
 					.setParameter(1, annee)
 					.setParameter(2, idligne);	        
-			 result =   ((BigInteger) query.uniqueResult()).intValue();	
+			result =   ((BigInteger) query.uniqueResult()).intValue();	
 		}
 		else if(affichage.equals("trimestre 3")){
 			se = HibernateUtils.getSession();
@@ -62,7 +62,7 @@ public class BiIncidentDAO {
 					.setParameter(0, idtype)
 					.setParameter(1, annee)
 					.setParameter(2, idligne);	        
-			 result =   ((BigInteger) query.uniqueResult()).intValue();	
+			result =   ((BigInteger) query.uniqueResult()).intValue();	
 		}
 		else if(affichage.equals("trimestre 4")){
 			se = HibernateUtils.getSession();
@@ -71,7 +71,7 @@ public class BiIncidentDAO {
 					.setParameter(0, idtype)
 					.setParameter(1, annee)
 					.setParameter(2, idligne);	        
-			 result =   ((BigInteger) query.uniqueResult()).intValue();	
+			result =   ((BigInteger) query.uniqueResult()).intValue();	
 		}
 		else if(affichage.equals("semestre 1")){
 			se = HibernateUtils.getSession();
@@ -80,7 +80,7 @@ public class BiIncidentDAO {
 					.setParameter(0, idtype)
 					.setParameter(1, annee)
 					.setParameter(2, idligne);	        
-			 result =   ((BigInteger) query.uniqueResult()).intValue();	
+			result =   ((BigInteger) query.uniqueResult()).intValue();	
 		}
 		else if(affichage.equals("semestre 2")){
 			se = HibernateUtils.getSession();
@@ -89,68 +89,252 @@ public class BiIncidentDAO {
 					.setParameter(0, idtype)
 					.setParameter(1, annee)
 					.setParameter(2, idligne);	        
-			 result =   ((BigInteger) query.uniqueResult()).intValue();	
+			result =   ((BigInteger) query.uniqueResult()).intValue();	
 		}
-		
+
 		return result;
 	}
-	public Time getTempsMoyenIncident(int idtype,int annee,int idligne, String affichage) {
-		
-		Time result= new Time(0,0,0);
+	public BigDecimal getTempsMoyenIncident(int idtype,int annee,int idligne, String affichage) {
+
+		BigDecimal result = new BigDecimal(0);
 		if(affichage.equals("annuelle")){
 			se = HibernateUtils.getSession();
 			Query query = se.createSQLQuery(
-					"SELECT SEC_TO_TIME( AVG( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) ) ) FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? and biligne_idbiligne= ? ")
+					"SELECT  AVG( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) )  FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? and biligne_idbiligne= ? ")
 					.setParameter(0, idtype)
 					.setParameter(1, annee)
 					.setParameter(2, idligne);	        
-			result =    (Time) query.uniqueResult();		
+			result =    (BigDecimal) query.uniqueResult();
+		}
+		else if(affichage.equals("trimestre 1")){
+			se = HibernateUtils.getSession();
+			Query query = se.createSQLQuery(
+					"SELECT  AVG( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) )  FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? AND mois between 1 and 3 AND biligne_idbiligne= ? ")
+					.setParameter(0, idtype)
+					.setParameter(1, annee)
+					.setParameter(2, idligne);	        
+			result =    (BigDecimal) query.uniqueResult();
+
+		}
+		else if(affichage.equals("trimestre 2")){
+			se = HibernateUtils.getSession();
+			Query query = se.createSQLQuery(
+					"SELECT  AVG( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) )  FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? AND mois between 4 and 6  AND biligne_idbiligne= ? ")
+					.setParameter(0, idtype)
+					.setParameter(1, annee)
+					.setParameter(2, idligne);	        
+			result =    (BigDecimal) query.uniqueResult();
+
+		}
+		else if(affichage.equals("trimestre 3")){
+			se = HibernateUtils.getSession();
+			Query query = se.createSQLQuery(
+					"SELECT  AVG( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) )  FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? AND mois between 7 and 9 AND biligne_idbiligne= ? ")
+					.setParameter(0, idtype)
+					.setParameter(1, annee)
+					.setParameter(2, idligne);	        
+			result =    (BigDecimal) query.uniqueResult();
+
+		}
+		else if(affichage.equals("trimestre 4")){
+			
+			se = HibernateUtils.getSession();
+			Query query = se.createSQLQuery(
+					"SELECT  AVG( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) )  FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? AND mois between 10 and 12 AND biligne_idbiligne= ? ")
+					.setParameter(0, idtype)
+					.setParameter(1, annee)
+					.setParameter(2, idligne);	        
+			result =    (BigDecimal) query.uniqueResult();
+
+		}
+		else if(affichage.equals("semestre 1")){
+			se = HibernateUtils.getSession();
+			Query query = se.createSQLQuery(
+					"SELECT  AVG( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) )  FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? AND mois between 1 and 6 AND biligne_idbiligne= ? ")
+					.setParameter(0, idtype)
+					.setParameter(1, annee)
+					.setParameter(2, idligne);	        
+			result =    (BigDecimal) query.uniqueResult();
+
+		}
+		else if(affichage.equals("semestre 2")){
+			se = HibernateUtils.getSession();
+			Query query = se.createSQLQuery(
+					"SELECT  AVG( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) )  FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? AND mois between 7 and 12 AND biligne_idbiligne= ? ")
+					.setParameter(0, idtype)
+					.setParameter(1, annee)
+					.setParameter(2, idligne);	        
+			result =    (BigDecimal) query.uniqueResult();
+
 		}
 
 		if(result!= null){return(result);}
 		else{
-			result= new Time(0,0,0);
+			result= new BigDecimal(0) ;
 			return(result);}
-		
+
 	}
-	
-	public Time getTempsMaxIncident(int idtype,int annee,int idligne, String affichage) {
-		
-		Time result= new Time(0,0,0);
+
+	public BigInteger getTempsMaxIncident(int idtype,int annee,int idligne, String affichage) {
+
+		BigInteger result= new BigInteger("0") ;
 		if(affichage.equals("annuelle")){
 			se = HibernateUtils.getSession();
 			Query query = se.createSQLQuery(
-					"SELECT SEC_TO_TIME( MAX( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) ) ) FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? and biligne_idbiligne= ? ")
+					"SELECT  MAX( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) )  FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? and biligne_idbiligne= ? ")
 					.setParameter(0, idtype)
 					.setParameter(1, annee)
 					.setParameter(2, idligne);	        
-			result =    (Time) query.uniqueResult();		
+			result =    (BigInteger) query.uniqueResult();			
+		}
+		else if(affichage.equals("trimestre 1")){
+			se = HibernateUtils.getSession();
+			Query query = se.createSQLQuery(
+					"SELECT  MAX( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) )  FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? AND mois between 1 and 3 AND biligne_idbiligne= ? ")
+					.setParameter(0, idtype)
+					.setParameter(1, annee)
+					.setParameter(2, idligne);	        
+			result =    (BigInteger) query.uniqueResult();
+
+		}
+		else if(affichage.equals("trimestre 2")){
+			se = HibernateUtils.getSession();
+			Query query = se.createSQLQuery(
+					"SELECT  MAX( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) )  FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? AND mois between 4 and 6  AND biligne_idbiligne= ? ")
+					.setParameter(0, idtype)
+					.setParameter(1, annee)
+					.setParameter(2, idligne);	        
+			result =    (BigInteger) query.uniqueResult();
+
+		}
+		else if(affichage.equals("trimestre 3")){
+			se = HibernateUtils.getSession();
+			Query query = se.createSQLQuery(
+					"SELECT  MAX( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) )  FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? AND mois between 7 and 9 AND biligne_idbiligne= ? ")
+					.setParameter(0, idtype)
+					.setParameter(1, annee)
+					.setParameter(2, idligne);	        
+			result =    (BigInteger) query.uniqueResult();
+
+		}
+		else if(affichage.equals("trimestre 4")){
+			
+			se = HibernateUtils.getSession();
+			Query query = se.createSQLQuery(
+					"SELECT  MAX( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) )  FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? AND mois between 10 and 12 AND biligne_idbiligne= ? ")
+					.setParameter(0, idtype)
+					.setParameter(1, annee)
+					.setParameter(2, idligne);	        
+			result =    (BigInteger) query.uniqueResult();
+
+		}
+		else if(affichage.equals("semestre 1")){
+			se = HibernateUtils.getSession();
+			Query query = se.createSQLQuery(
+					"SELECT  MAX( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) )  FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? AND mois between 1 and 6 AND biligne_idbiligne= ? ")
+					.setParameter(0, idtype)
+					.setParameter(1, annee)
+					.setParameter(2, idligne);	        
+			result =    (BigInteger) query.uniqueResult();
+
+		}
+		else if(affichage.equals("semestre 2")){
+			se = HibernateUtils.getSession();
+			Query query = se.createSQLQuery(
+					"SELECT  MAX( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) )  FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? AND mois between 7 and 12 AND biligne_idbiligne= ? ")
+					.setParameter(0, idtype)
+					.setParameter(1, annee)
+					.setParameter(2, idligne);	        
+			result =    (BigInteger) query.uniqueResult();
+
 		}
 
 		if(result!= null){return(result);}
 		else{
-			result= new Time(0,0,0);
+			result=	new BigInteger("0") ;
 			return(result);}
-		
+
 	}
-public Time getTempsMinIncident(int idtype,int annee,int idligne, String affichage) {
-		
-		Time result= new Time(0,0,0);
+	public BigInteger getTempsMinIncident(int idtype,int annee,int idligne, String affichage) {
+
+		BigInteger result= new BigInteger("0") ;
 		if(affichage.equals("annuelle")){
 			se = HibernateUtils.getSession();
 			Query query = se.createSQLQuery(
-					"SELECT SEC_TO_TIME( Min( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) ) ) FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? and biligne_idbiligne= ? ")
+					"SELECT  MIN( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) )  FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? and biligne_idbiligne= ? ")
 					.setParameter(0, idtype)
 					.setParameter(1, annee)
 					.setParameter(2, idligne);	        
-			result =    (Time) query.uniqueResult();		
+			result =    (BigInteger) query.uniqueResult();		
 		}
+		else if(affichage.equals("trimestre 1")){
+			se = HibernateUtils.getSession();
+			Query query = se.createSQLQuery(
+					"SELECT  MIN( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) )  FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? AND mois between 1 and 3 AND biligne_idbiligne= ? ")
+					.setParameter(0, idtype)
+					.setParameter(1, annee)
+					.setParameter(2, idligne);	        
+			result =    (BigInteger) query.uniqueResult();
+
+		}
+		else if(affichage.equals("trimestre 2")){
+			se = HibernateUtils.getSession();
+			Query query = se.createSQLQuery(
+					"SELECT  MIN( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) )  FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? AND mois between 4 and 6  AND biligne_idbiligne= ? ")
+					.setParameter(0, idtype)
+					.setParameter(1, annee)
+					.setParameter(2, idligne);	        
+			result =    (BigInteger) query.uniqueResult();
+
+		}
+		else if(affichage.equals("trimestre 3")){
+			se = HibernateUtils.getSession();
+			Query query = se.createSQLQuery(
+					"SELECT  MIN( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) )  FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? AND mois between 7 and 9 AND biligne_idbiligne= ? ")
+					.setParameter(0, idtype)
+					.setParameter(1, annee)
+					.setParameter(2, idligne);	        
+			result =    (BigInteger) query.uniqueResult();
+
+		}
+		else if(affichage.equals("trimestre 4")){
+			
+			se = HibernateUtils.getSession();
+			Query query = se.createSQLQuery(
+					"SELECT  MIN( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) )  FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? AND mois between 10 and 12 AND biligne_idbiligne= ? ")
+					.setParameter(0, idtype)
+					.setParameter(1, annee)
+					.setParameter(2, idligne);	        
+			result =    (BigInteger) query.uniqueResult();
+
+		}
+		else if(affichage.equals("semestre 1")){
+			se = HibernateUtils.getSession();
+			Query query = se.createSQLQuery(
+					"SELECT  MIN( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) )  FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? AND mois between 1 and 6 AND biligne_idbiligne= ? ")
+					.setParameter(0, idtype)
+					.setParameter(1, annee)
+					.setParameter(2, idligne);	        
+			result =    (BigInteger) query.uniqueResult();
+
+		}
+		else if(affichage.equals("semestre 2")){
+			se = HibernateUtils.getSession();
+			Query query = se.createSQLQuery(
+					"SELECT  MIN( TIME_TO_SEC( TIMEDIFF( datefin, datedebut ) ) )  FROM incident WHERE datefin IS NOT NULL AND type_idtype= ? AND annee= ? AND mois between 7 and 12 AND biligne_idbiligne= ? ")
+					.setParameter(0, idtype)
+					.setParameter(1, annee)
+					.setParameter(2, idligne);	        
+			result =    (BigInteger) query.uniqueResult();
+
+		}
+
 
 		if(result!= null){return(result);}
 		else{
-			result= new Time(0,0,0);
+			result=new BigInteger("0") ; ;
 			return(result);}
-		
+
 	}
-	
+
 }
